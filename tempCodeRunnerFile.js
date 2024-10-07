175cc4146d4864aa8feccd13a0f9ca99addb9ca1
dev_irene <irene.nzole@gmail.com>
2024-10-07 16:46:23 +0300

    frst commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+document.querySelector("a.ham-icon").addEventListener("click", function(event){
+    document.getElementById("sideNavigation").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  });
